70b587de6be4222e40865cc00bbef7aa91722a42
noct4 <baileyivancic@icloud.com>
2021-02-15 15:21:29 +1100

    Main in progress

--- access-controller/access-controller-endpoints/src/main/java/oxfordscholar/services/accesscontroller/app/Main.java
+++ access-controller/access-controller-endpoints/src/main/java/oxfordscholar/services/accesscontroller/app/Main.java
@@ -1,13 +1,20 @@
 package oxfordscholar.services.accesscontroller.app;
 
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
 
 public class Main {
 
 	public static void main(String[] args) {
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
 
+		context.setContextPath("/");
+		AppConfig appConfig = new AppConfig();
 		
+		ServletContainer jerseyContainer = new ServletContainer(appConfig);
+		
+		ServletHolder servletHolder = new ServletHolder(jerseyContainer);
 	}
 
 }
