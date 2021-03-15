9db359f6ac1fe2a762d4be1bab4809f84448be73
noct4.dev <dev@noct4>
2021-03-15 15:23:59 +1100

    Fix

--- access-controller/access-controller-endpoints/src/main/java/oxfordscholar/services/accesscontroller/app/AppConfig.java
+++ access-controller/access-controller-endpoints/src/main/java/oxfordscholar/services/accesscontroller/app/AppConfig.java
@@ -52,11 +52,12 @@ public class AppConfig extends ResourceConfig
 			fileName = System.getenv("APPLICATION_PROP_FILE");
 		}
 		
-		File configFile = new File(fileName);
-
 		
-		try (Scanner scanner = new Scanner(configFile);)
+		try
 		{
+			File configFile = new File(fileName);
+			Scanner scanner = new Scanner(configFile);
+			
 			while (scanner.hasNextLine())
 			{
 				String line = scanner.nextLine();
