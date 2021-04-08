db48ea97bccaecd49cb9ae4ded82345db29c549b
noct4.dev <dev@noct4>
2021-04-08 21:03:17 +1000

    Up to date

--- gui/routes/sql.js
+++ gui/routes/sql.js
@@ -4,9 +4,10 @@ const express = require('express');
 
 const router = express.Router();
 
-// See if we can hit the SQL server
 router.get('/', (req, res) => {
     res.send("welcome to sql");
 });
+
+
 
 module.exports = router;
--- sql/sql.bash
+++ sql/sql.bash
@@ -1,2 +1,2 @@
 docker build -t sqlrest .;
-docker run -d --name sqlcontainer -it -p 8090:3306 -e MYSQL_ROOT_PASSWORD=IckyDitto9 sqlrest;
+docker run -d --name sqlcontainer -it -p 3306:3306 -e MYSQL_ROOT_PASSWORD=OxfordScholar1! sqlrest;
